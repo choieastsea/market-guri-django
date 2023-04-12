6a65b2ba32b52d258aacb3e2789722aacf00fcaa
choieastsea <choieastsea@gmail.com>
2023-04-12 23:12:40 +0900

    [FEAT] item model migration

--- item/models.py
+++ item/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    item_id = models.AutoField(primary_key=True, verbose_name='상품코드')
+    name = models.CharField(max_length=100, verbose_name='상품명')
+    price = models.PositiveIntegerField(verbose_name='상품가격')
+    stock_count = models.PositiveIntegerField(verbose_name='남은수량')
+
+    class Meta:
+        db_table = 'item'
+        verbose_name = '상품'
+        verbose_name_plural = '상품들'
